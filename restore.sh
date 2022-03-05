ed333939449a27b773e100d3b757cca314549443
nghoanglong <19521788@gm.uit.edu.vn>
2022-03-05 17:41:58 +0700

    init git rules

---
+++ restore.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git restore .idea/compiler.xml
+git restore .idea/gradle.xml
+git restore .idea/misc.xml
+git restore build.gradle
+git restore gradle/wrapper/gradle-wrapper.jar
+git restore gradle/wrapper/gradle-wrapper.properties
+git restore gradlew
