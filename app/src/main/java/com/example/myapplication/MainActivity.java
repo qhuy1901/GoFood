c19eaee8931866eca6f35c3ba16a16fc466a7c8e
Quang Huy <62685126+qhuy1901@users.noreply.github.com>
2022-03-07 21:22:01 +0700

    code logout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,26 +1,68 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvHello;
+    private Button btnLogout;
 
-
+    private void initUI()
+    {
+        // Khai báo các thành phần trong View
+        tvHello = (TextView) findViewById(R.id.tvHello);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
 
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference("message");
-//
-//        myRef.setValue("Hello, World!");
+        FirebaseDatabase goFoodDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = goFoodDatabase.getReference("Users");
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        String userId = user.getUid();
+        myRef.child(userId).addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                tvHello.setText("Xin chào " + snapshot.child("FullName").getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+               // Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+
+        // Xử lý sự kiện btn Logout
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent switchActivityIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(switchActivityIntent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -20,43 +20,81 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 
 public class RegisterActivity extends AppCompatActivity {
+    private Button btnRegister;
+    private EditText edtFullName;
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private EditText edtConfirmPassword;
+
+    private void initUI()
+    {
+        // Khai báo các thành phần trong View
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        edtFullName = (EditText) findViewById(R.id.edtFullName);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        edtConfirmPassword = (EditText) findViewById(R.id.edtConfirmPassword);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initUI();
 
-        FirebaseAuth mAuth;
-        mAuth = FirebaseAuth.getInstance();
+        // Khai báo các util của Firebase
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseDatabase goFoodDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = goFoodDatabase.getReference("Users");
 
-        // Khai báo các thành phần trong View
-        Button btnRegister = (Button) findViewById(R.id.btnRegister);
-        EditText edtEmail = (EditText) findViewById(R.id.edtEmail);
-        EditText edtPassword = (EditText) findViewById(R.id.edtPassword);
         ProgressDialog progressDialog = new ProgressDialog(this);
 
-        // Xử lý sự kiện
+        // Xử lý sự kiện click button Đăng kí
         btnRegister.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v) {
+
                 String email = edtEmail.getText().toString();
                 String password = edtPassword.getText().toString();
-                progressDialog.show();
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(RegisterActivity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
-                                    Intent switchActivityIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                                    startActivity(switchActivityIntent);
-                                    finishAffinity();
-                                } else {
-                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+                String confirmPassword = edtConfirmPassword.getText().toString();
+                String fullName = edtFullName.getText().toString();
+
+                if(confirmPassword.equals(password))
+                {
+
+                    progressDialog.show();
+
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        String newUserId = mAuth.getCurrentUser().getUid();
+                                        myRef.child(newUserId).child("FullName").setValue(fullName).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(RegisterActivity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                                                    Intent switchActivityIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                                                    startActivity(switchActivityIntent);
+                                                    finishAffinity();
+                                                } else {
+                                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                                    }
+                                    else {
+                                        Toast.makeText(RegisterActivity.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+                                    }
                                 }
-                            }
-                        });
+                            });
+                }
+                else
+                {
+                    Toast.makeText(RegisterActivity.this, "Xác nhận mật khẩu không đúng! Vui lòng nhập lại.",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
