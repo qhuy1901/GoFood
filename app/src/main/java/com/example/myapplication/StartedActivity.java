e94b58d463bee63f2f5605a76a282ffc782b3084
Le Thi Hong Cuc <cuclth2701@gmail.com>
2022-03-16 16:27:14 +0700

    FE Started

--- app/src/main/java/com/example/myapplication/StartedActivity.java
+++ app/src/main/java/com/example/myapplication/StartedActivity.java
@@ -5,14 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class StartedActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN,);
         setContentView(R.layout.activity_started);
+
+        //Animations
+        Animation topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        Animation botAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        Animation fadeAnim = AnimationUtils.loadAnimation(this, R.anim.fade_anim);
+
+        ImageView image = findViewById(R.id.imageView2);
+        TextView slogan = findViewById(R.id.textView);
+        ImageView img = findViewById(R.id.imageView);
+
+        image.setAnimation(topAnim);
+        slogan.setAnimation(topAnim);
+        img.setAnimation(fadeAnim);
 
 //        Button btnGetStarted = (Button) findViewById(R.id.btnGetStarted);
 //
