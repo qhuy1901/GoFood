88dc85985bf76b360b0306efb659e3fcc2efe10d
Le Thi Hong Cuc <cuclth2701@gmail.com>
2022-03-16 21:02:44 +0700

    Fix started page

--- app/src/main/java/com/example/myapplication/StartedActivity.java
+++ app/src/main/java/com/example/myapplication/StartedActivity.java
@@ -18,7 +18,7 @@ public class StartedActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN,);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_started);
 
         //Animations
