db63b88f16db524c054311719381b6900779b6ca
nghoanglong <19521788@gm.uit.edu.vn>
2022-03-29 23:52:52 +0700

    sua singup, add homepage

---
+++ app/src/main/java/com/example/myapplication/models/PopularProductModel.java
@@ -0,0 +1,38 @@
+package com.example.myapplication.models;
+
+public class PopularProductModel {
+    String img_url;
+    String name;
+    String type;
+
+    public PopularProductModel(){ }
+    public PopularProductModel(String img_url, String name, String type){
+        this.img_url = img_url;
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
