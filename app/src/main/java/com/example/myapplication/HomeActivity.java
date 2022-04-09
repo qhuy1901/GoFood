3c30199551d92fdf32f90b971f91822361f58da8
nghoanglong <19521788@gm.uit.edu.vn>
2022-04-09 21:30:45 +0700

    update fragment homepage

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -2,20 +2,28 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.example.myapplication.adapter.Home_AllCateAdapter;
 import com.example.myapplication.adapter.Home_CategoriesOrderAdapter;
 import com.example.myapplication.adapter.Home_RecommendedAdapter;
+import com.example.myapplication.customer.home.CateHomeFragment;
+import com.example.myapplication.customer.home.CateOrderFragment;
 import com.example.myapplication.models.Home_CategoriesOrderModel;
 import com.example.myapplication.models.Home_MenuCategoriesModel;
 import com.example.myapplication.models.Home_RecommendedOrderModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -30,127 +38,34 @@ import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private TextView tvLocation;
-
-    DatabaseReference realtimedbRef;
-    FirebaseFirestore fire_store = FirebaseFirestore.getInstance();;
-
-    RecyclerView categories_order_RecyclerView;
-    Home_CategoriesOrderAdapter categories_order_Adapter;
-    List<Home_CategoriesOrderModel> categories_order_ModelList;
-
-    RecyclerView recommended_order_RecyclerView;
-    Home_RecommendedAdapter recommended_order_Adapter;
-    List<Home_RecommendedOrderModel> recommended_order_ModelList;
-
-    RecyclerView allcate_menu_RecyclerView;
-    Home_AllCateAdapter allcate_menu_Adapter;
-    List<Home_MenuCategoriesModel> allcate_menu_ModelList;
+    BottomNavigationView btnBottom_Nav_HomeID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        tvLocation = (TextView) findViewById(R.id.tvLocation);
-        if(WelcomeActivity.type_usr == 1) {
+        btnBottom_Nav_HomeID = findViewById(R.id.bottom_nav_home);
+        setFragment(new CateHomeFragment());
+        btnBottom_Nav_HomeID.setSelectedItemId(R.id.nav_home);
 
-            // get location
-            getLocation();
-
-            // get recommended order
-            getRecommendedOrder();
-
-            // categories order
-            getCategoriesOrder();
-
-            // category - allcate
-            getAllCateMenu();
-        }else{
-            tvLocation.setText("MERCHANT LOGIN");
-        }
-    }
-
-    private void getCategoriesOrder(){
-        categories_order_RecyclerView = findViewById(R.id.categories_order_rec);
-        categories_order_RecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
-        categories_order_ModelList = new ArrayList<>();
-        categories_order_Adapter = new Home_CategoriesOrderAdapter(this, categories_order_ModelList);
-        categories_order_RecyclerView.setAdapter(categories_order_Adapter);
-
-        fire_store.collection("Categories_Order")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful()){
-                            for (QueryDocumentSnapshot document: task.getResult()){
-                                Home_CategoriesOrderModel categories_order = document.toObject(Home_CategoriesOrderModel.class);
-                                categories_order_ModelList.add(categories_order);
-                                categories_order_Adapter.notifyDataSetChanged();
-                            }
-                        }
-                    }
-                });
-    }
-
-    private void getRecommendedOrder(){
-        recommended_order_RecyclerView = findViewById(R.id.recommended_rec);
-        recommended_order_RecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
-        recommended_order_ModelList = new ArrayList<>();
-        recommended_order_Adapter = new Home_RecommendedAdapter(this, recommended_order_ModelList);
-        recommended_order_RecyclerView.setAdapter(recommended_order_Adapter);
-
-        fire_store.collection("Recommended_Order")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful()){
-                            for (QueryDocumentSnapshot document: task.getResult()){
-                                Home_RecommendedOrderModel recommended_order = document.toObject(Home_RecommendedOrderModel.class);
-                                recommended_order_ModelList.add(recommended_order);
-                                recommended_order_Adapter.notifyDataSetChanged();
-                            }
-                        }
-                    }
-                });
-    }
-
-    private void getAllCateMenu(){
-        allcate_menu_RecyclerView = findViewById(R.id.menucate_rec);
-        allcate_menu_RecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
-        allcate_menu_ModelList = new ArrayList<>();
-        allcate_menu_Adapter = new Home_AllCateAdapter(this, allcate_menu_ModelList);
-        allcate_menu_RecyclerView.setAdapter(allcate_menu_Adapter);
-
-        fire_store.collection("AllCate_Menu")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful()){
-                            for (QueryDocumentSnapshot document: task.getResult()){
-                                Home_MenuCategoriesModel allcate = document.toObject(Home_MenuCategoriesModel.class);
-                                allcate_menu_ModelList.add(allcate);
-                                allcate_menu_Adapter.notifyDataSetChanged();
-                            }
-                        }
-                    }
-                });
-    }
-
-    private void getLocation(){
-        realtimedbRef = FirebaseDatabase.getInstance().getReference("Users");
-        realtimedbRef.child(LoginTabFragment.UID).child("cur_location").addValueEventListener(new ValueEventListener() {
+        btnBottom_Nav_HomeID.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String cur_location = snapshot.getValue(String.class);
-                tvLocation.setText(cur_location);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        setFragment(new CateHomeFragment());
+                        return true;
+                    case R.id.nav_my_ords:
+                        setFragment(new CateOrderFragment());
+                        return true;
+                }
+                return false;
             }
         });
+    }
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.home_framelayout, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/myapplication/customer/home/CateHomeFragment.java
@@ -0,0 +1,165 @@
+package com.example.myapplication.customer.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.myapplication.LoginTabFragment;
+import com.example.myapplication.R;
+import com.example.myapplication.WelcomeActivity;
+import com.example.myapplication.adapter.Home_AllCateAdapter;
+import com.example.myapplication.adapter.Home_CategoriesOrderAdapter;
+import com.example.myapplication.adapter.Home_RecommendedAdapter;
+import com.example.myapplication.models.Home_CategoriesOrderModel;
+import com.example.myapplication.models.Home_MenuCategoriesModel;
+import com.example.myapplication.models.Home_RecommendedOrderModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CateHomeFragment extends Fragment {
+
+
+    private TextView tvLocation;
+    DatabaseReference realtimedbRef;
+    FirebaseFirestore fire_store = FirebaseFirestore.getInstance();;
+
+    RecyclerView categories_order_RecyclerView;
+    Home_CategoriesOrderAdapter categories_order_Adapter;
+    List<Home_CategoriesOrderModel> categories_order_ModelList;
+
+    RecyclerView recommended_order_RecyclerView;
+    Home_RecommendedAdapter recommended_order_Adapter;
+    List<Home_RecommendedOrderModel> recommended_order_ModelList;
+
+    RecyclerView allcate_menu_RecyclerView;
+    Home_AllCateAdapter allcate_menu_Adapter;
+    List<Home_MenuCategoriesModel> allcate_menu_ModelList;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_catehome, container, false);
+        tvLocation = (TextView) root.findViewById(R.id.tvLocation);
+        if(WelcomeActivity.type_usr == 1) {
+
+            // get location
+            getLocation();
+
+            // get recommended order
+            getRecommendedOrder(root);
+
+            // categories order
+            getCategoriesOrder(root);
+
+            // category - allcate
+            getAllCateMenu(root);
+        }else{
+            tvLocation.setText("MERCHANT LOGIN");
+        }
+        // Inflate the layout for this fragment
+        return root;
+    }
+    private void getCategoriesOrder(ViewGroup view){
+        categories_order_RecyclerView = view.findViewById(R.id.categories_order_rec);
+        categories_order_RecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        categories_order_ModelList = new ArrayList<>();
+        categories_order_Adapter = new Home_CategoriesOrderAdapter(getActivity(), categories_order_ModelList);
+        categories_order_RecyclerView.setAdapter(categories_order_Adapter);
+
+        fire_store.collection("Categories_Order")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot document: task.getResult()){
+                                Home_CategoriesOrderModel categories_order = document.toObject(Home_CategoriesOrderModel.class);
+                                categories_order_ModelList.add(categories_order);
+                                categories_order_Adapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void getRecommendedOrder(ViewGroup view){
+        recommended_order_RecyclerView = view.findViewById(R.id.recommended_rec);
+        recommended_order_RecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        recommended_order_ModelList = new ArrayList<>();
+        recommended_order_Adapter = new Home_RecommendedAdapter(getActivity(), recommended_order_ModelList);
+        recommended_order_RecyclerView.setAdapter(recommended_order_Adapter);
+
+        fire_store.collection("Recommended_Order")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot document: task.getResult()){
+                                Home_RecommendedOrderModel recommended_order = document.toObject(Home_RecommendedOrderModel.class);
+                                recommended_order_ModelList.add(recommended_order);
+                                recommended_order_Adapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void getAllCateMenu(ViewGroup view){
+        allcate_menu_RecyclerView = view.findViewById(R.id.menucate_rec);
+        allcate_menu_RecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false));
+        allcate_menu_ModelList = new ArrayList<>();
+        allcate_menu_Adapter = new Home_AllCateAdapter(getActivity(), allcate_menu_ModelList);
+        allcate_menu_RecyclerView.setAdapter(allcate_menu_Adapter);
+
+        fire_store.collection("AllCate_Menu")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot document: task.getResult()){
+                                Home_MenuCategoriesModel allcate = document.toObject(Home_MenuCategoriesModel.class);
+                                allcate_menu_ModelList.add(allcate);
+                                allcate_menu_Adapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void getLocation(){
+        realtimedbRef = FirebaseDatabase.getInstance().getReference("Users");
+        realtimedbRef.child(LoginTabFragment.UID).child("cur_location").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String cur_location = snapshot.getValue(String.class);
+                tvLocation.setText(cur_location);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+}
